2aea6728194015ba24f5e81ef3fe4e90c1828fec
Alexandr Kukuruza <cornalexandr@gmail.com>
2021-05-31 02:25:02 +0300

    1

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    product: [
+        {
+            _id: '0',
+            name: 'Ноутбук ASUS X515MA-BR062',
+            category: 'Notebook',
+            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
+            price: 1089,
+            countInStock: 0,
+            brand: 'ASUS',
+            rating: 4.5,
+            numReviews: 14,
+            description: '15.6" (1366 x 768 матовый)'
+        },
+        {
+            _id: '1',
+            name: 'Ноутбук ASUS X515MA-BR062',
+            category: 'Notebook',
+            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
+            price: 1089,
+            countInStock: 10,
+            brand: 'ASUS',
+            rating: 4.5,
+            numReviews: 7,
+            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
+        },
+        {
+            _id: '2',
+            name: 'Ноутбук ASUS X515MA-BR062',
+            category: 'Notebook',
+            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
+            price: 1089,
+            countInStock: 10,
+            brand: 'ASUS',
+            rating: 5,
+            numReviews: 120,
+            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
+        },
+        {
+            _id: '3',
+            name: 'Ноутбук ASUS X515MA-BR062',
+            category: 'Notebook',
+            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
+            price: 19,
+            countInStock: 10,
+            brand: 'ASUS',
+            rating: 4,
+            numReviews: 5,
+            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
+        },
+        {
+            _id: '4',
+            name: 'Ноутбук ASUS X515MA-BR062',
+            category: 'Notebook',
+            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
+            price: 1089,
+            countInStock: 10,
+            brand: 'ASUS',
+            rating: 4,
+            numReviews: 2,
+            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
+        },
+        {
+            _id: '5',
+            name: 'Ноутбук ASUS X515MA-BR062',
+            category: 'Notebook',
+            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
+            price: 1029,
+            countInStock: 2,
+            brand: 'ASUS',
+            rating: 5,
+            numReviews: 1,
+            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
+        },
+    ],
+};
+export default data;
