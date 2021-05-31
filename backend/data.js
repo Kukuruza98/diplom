e2d400fbc1a303ccbe7e608b0a914959840fac91
Alexandr Kukuruza <cornalexandr@gmail.com>
2021-05-31 18:32:35 +0300

    share code on github

--- backend/data.js
+++ backend/data.js
@@ -1,77 +1,93 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-    products: [
-        {
-            _id: '0',
-            name: 'Ноутбук ASUS X515MA-BR062',
-            category: 'Notebook',
-            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
-            price: 1089,
-            countInStock: 0,
-            brand: 'ASUS',
-            rating: 4.5,
-            numReviews: 14,
-            description: '15.6" (1366 x 768 матовый)'
-        },
-        {
-            _id: '1',
-            name: 'Ноутбук ASUS X515MA-BR062',
-            category: 'Notebook',
-            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
-            price: 1089,
-            countInStock: 10,
-            brand: 'ASUS',
-            rating: 4.5,
-            numReviews: 7,
-            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
-        },
-        {
-            _id: '2',
-            name: 'Ноутбук ASUS X515MA-BR062',
-            category: 'Notebook',
-            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
-            price: 1089,
-            countInStock: 10,
-            brand: 'ASUS',
-            rating: 5,
-            numReviews: 120,
-            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
-        },
-        {
-            _id: '3',
-            name: 'Ноутбук ASUS X515MA-BR062',
-            category: 'Notebook',
-            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
-            price: 19,
-            countInStock: 10,
-            brand: 'ASUS',
-            rating: 4,
-            numReviews: 5,
-            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
-        },
-        {
-            _id: '4',
-            name: 'Ноутбук ASUS X515MA-BR062',
-            category: 'Notebook',
-            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
-            price: 1089,
-            countInStock: 10,
-            brand: 'ASUS',
-            rating: 4,
-            numReviews: 2,
-            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
-        },
-        {
-            _id: '5',
-            name: 'Ноутбук ASUS X515MA-BR062',
-            category: 'Notebook',
-            image: '/images/noutbuk-asus-x515ma-br062-680px-449px.jpg',
-            price: 1029,
-            countInStock: 2,
-            brand: 'ASUS',
-            rating: 5,
-            numReviews: 1,
-            description: '15.6" (1366 x 768 матовый), Intel Celeron N4020 (1 100 МГц), 4 ГБ (DDR4), 256 ГБ (SSD), встроенный, привод: нет, серый'
-        },
-    ],
+  users: [
+    {
+      name: 'Alex',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Mari',
+      email: 'mari@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+
+      name: 'Маша Амбражевич',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+
+      name: 'Adidas Fit Shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 100,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+
+      name: 'Lacoste Free Shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 220,
+      countInStock: 0,
+      brand: 'Lacoste',
+      rating: 4.8,
+      numReviews: 17,
+      description: 'high quality product',
+    },
+    {
+
+      name: 'Nike Slim Pant',
+      category: 'Pants',
+      image: '/images/p1.jpg',
+      price: 78,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+
+      name: 'Puma Slim Pant',
+      category: 'Pants',
+      image: '/images/p1.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+
+      name: 'Adidas Fit Pant',
+      category: 'Pants',
+      image: '/images/p1.jpg',
+      price: 139,
+      countInStock: 12,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality product',
+    },
+  ],
 };
 export default data;
